2456b36ff4710faf39f9d73bde65001dab0ab240
jypark0407 <jypark04070@gmail.com>
2014-12-06 15:45:01 -0500

    test
    test

---
+++ de.vogella.eclipse.ide.first/src/de/vogella/eclipse/ide/first/MyFirstClass.java
@@ -0,0 +1,11 @@
+package de.vogella.eclipse.ide.first;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Eclipse!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
